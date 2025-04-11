cfb41703374031b8777435afe2b09d35f90774c7
Dudi Likvornik <likdudi@gmail.com>
2025-04-11 16:47:21 +0300

    more changes.

---
+++ Common/StateMachine/State.cs
@@ -0,0 +1,9 @@
+﻿namespace Argus.Common.StateMachine;
+
+public abstract class State<TEdge, TArg1, TArg2, TResult> where TEdge : Enum
+{
+    public virtual TResult HandleState(StateContext<TEdge, TArg1, TArg2, TResult> context, TEdge command, TArg1 argument1, TArg2 argument2)
+    {
+        throw new InvalidOperationException();
+    }
+}
---
+++ Common/StateMachine/StateContext.cs
@@ -0,0 +1,23 @@
+﻿namespace Argus.Common.StateMachine;
+
+public class StateContext<TEdge, TArg1, TArg2, TResult> where TEdge : Enum
+{
+    private State<TEdge, TArg1, TArg2, TResult> _currentState;
+
+    public StateContext(State<TEdge, TArg1, TArg2, TResult> startingState)
+    {
+        _currentState = startingState;
+    }
+
+    public TResult HandleState(TEdge commandType, TArg1 argument1, TArg2 argument2)
+    {
+        return _currentState.HandleState(this, commandType, argument1, argument2);
+    }
+
+    public void SetState(State<TEdge, TArg1, TArg2, TResult> nextState)
+    {
+        _currentState = nextState;
+    }
+
+}
+
